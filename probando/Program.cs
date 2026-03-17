32ac67bfc21ec42005effc47ce54afd0d76d51df
Miguel Rivera <166536996+mriverawm@users.noreply.github.com>
2026-03-17 17:46:03 -0500

    Se ajusta api key para prueba
    Updated the Stripe API key to a new test key.

--- probando/Program.cs
+++ probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Usuario!");
 
-string stripe_api_key = "sadsafbref213235gsd"; 
+string stripe_api_key="sk_test_4cjk345klj34oisdfon3n"
